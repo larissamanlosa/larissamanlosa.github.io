5fa3ffacbbe554c850fecf843e7fd88974625290
larissamanlosa <35215109+larissamanlosa@users.noreply.github.com>
2018-03-03 13:08:06 -0800

    Add files via upload

--- module06.js
+++ module06.js
@@ -6,7 +6,40 @@
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 upRight(pHeight, pColorEven, pColorOdd, pSymbol);
 downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+upLeft (pHeight, pColorEven, pColorOdd, pSymbol);
+downLeft (pHeight, pColorEven, pColorOdd, pSymbol);
 
+}
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=1; i<=pHeight;i++){
+rLine +="<p>";
+
+//Fill in the left side of every row with spaces or blanks
+
+for (x=1; x<=pHeight-i; x++){
+     // rLine+="-";
+  rLine +="<span class='space'>" + pSymbol +"</span>";
+}
+
+//Create each line on the Rhombus
+for(j=x;j<=pHeight;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+document.getElementById("upLeft").innerHTML = rLine;
 }
 
 
@@ -34,37 +67,6 @@ rLine +="</p>";
 document.getElementById("upRight").innerHTML = rLine;
 }
 
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=1; i<=pHeight;i++){
-rLine +="<p>";
-
-//Fill in the left side of every row with spaces or blanks
-
-for (x=1; x<=pHeight-i; x++){
-      rLine+="-";
-}
-
-//Create each line on the Rhombus
-for(j=x;j<=pHeight;j++){
-
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("upLeft").innerHTML = rLine;
-}
-
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=pHeight;i > 0;i--){
@@ -89,3 +91,33 @@ rLine +="</p>";
 document.getElementById("downRight").innerHTML = rLine;
 }
 
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+
+//Fill in the left side of every row with spaces or blanks
+
+for (x=1; x<=pHeight-i; x++){
+     // rLine+="-";
+  rLine +="<span class='space'>" + pSymbol +"</span>";
+}
+
+//Create each line on the Rhombus
+for(j=0;j<i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+document.getElementById("downLeft").innerHTML = rLine;
+}
